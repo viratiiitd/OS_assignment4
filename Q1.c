e0c6018ed448b991101e3cc24ffd6526a32f7ecd
viratiiitd <144791205+viratiiitd@users.noreply.github.com>
2023-11-08 20:36:35 +0530

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -3,8 +3,10 @@
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
+
+
 void *philosopher(void *arg){
-    int i;
+    int i,shared_variable = 0 ;
     for (i = 0; i < 10; i++) {
         // Acquire the lock
         pthread_mutex_lock(&mutex);
@@ -20,15 +22,24 @@ void *philosopher(void *arg){
 }
 
 int main() {
-    pthread_t thread1, thread2;
+    pthread_t thread0, thread1, thread2, thread3, thread4;
 
     // Create two threads
-    pthread_create(&thread1, NULL, philosopher, (void*)1);
-    pthread_create(&thread2, NULL, philosopher, (void*)2);
+    for(int i = 0 ; i < 5 ; i++){
+        pthread_create(&thread0, NULL, philosopher, (void*)i);
+        pthread_create(&thread1, NULL, philosopher, (void*)i);
+        pthread_create(&thread2, NULL, philosopher, (void*)i);
+        pthread_create(&thread3, NULL, philosopher, (void*)i);
+        pthread_create(&thread4, NULL, philosopher, (void*)i);
+    }
+
 
     // Wait for threads to finish
+    pthread_join(thread0, NULL);
     pthread_join(thread1, NULL);
     pthread_join(thread2, NULL);
+    pthread_join(thread3, NULL);
+    pthread_join(thread4, NULL);
 
     return 0;
 }
