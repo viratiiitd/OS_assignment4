b5ba6ef4da6febc77b26443dc58463b3d2ab63a2
viratiiitd <144791205+viratiiitd@users.noreply.github.com>
2023-11-21 21:41:54 +0530

    Update Q1.c

--- Q1.c
+++ Q1.c
@@ -1,6 +1,7 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 #define N 5
 #define THINKING 2
@@ -8,12 +9,21 @@
 #define EATING 0
 
 pthread_mutex_t mutex;
-pthread_cond_t cond[N];
-pthread_mutex_t bowl_mutex;
 pthread_cond_t bowl_cond;
-int state[N];
-int phil[N] = {0, 1, 2, 3, 4};
-int bowls_available = 2;
+
+int state[N]; // Added state array
+pthread_mutex_t bowl_locks[N]; // Added locks for each bowl
+
+struct Philosopher
+{
+    int id;
+    pthread_t thread_id;
+    pthread_cond_t cond;
+    struct Philosopher *next;
+    struct Philosopher *prev; // Added prev pointer for doubly linked list
+};
+
+struct Philosopher *philosophers;
 
 void eating(int phnum)
 {
@@ -27,100 +37,143 @@ void thinking(int phnum)
     sleep(1);
 }
 
-void test(int phnum)
+void test(struct Philosopher *phil)
 {
-    if (state[phnum] == HUNGRY && state[(phnum + 1) % N] != EATING && state[(phnum + 2) % N] != EATING && bowls_available > 0)
+    if (state[phil->id] == HUNGRY &&
+        state[phil->next->id] != EATING &&
+        state[phil->prev->id] != EATING)
     {
         // state that eating
-        state[phnum] = EATING;
+        state[phil->id] = EATING;
 
-        pthread_cond_signal(&cond[phnum]);
-        bowls_available--;
+        pthread_cond_signal(&phil->cond);
     }
 }
 
-void take_fork(int phnum)
+void take_fork(struct Philosopher *phil)
 {
     pthread_mutex_lock(&mutex);
 
     // state that hungry
-    state[phnum] = HUNGRY;
+    state[phil->id] = HUNGRY;
 
-    printf("Philosopher %d is Hungry\n", phnum + 1);
+    // Pick up both forks
+    test(phil);
 
-    // eat if neighbors are not eating and there is a bowl available
-    test(phnum);
-
-    while (state[phnum] != EATING)
-        pthread_cond_wait(&cond[phnum], &mutex);
+    while (state[phil->id] != EATING)
+        pthread_cond_wait(&phil->cond, &mutex);
 
     pthread_mutex_unlock(&mutex);
 }
 
-void put_fork(int phnum)
+void put_fork(struct Philosopher *phil)
 {
     pthread_mutex_lock(&mutex);
 
     // idhar thinking aaega
-    state[phnum] = THINKING;
+    state[phil->id] = THINKING;
 
-    
-	printf("\n");
-
-    test((phnum + 1) % N);
-    test((phnum + 2) % N);
+    test(phil->next);
+    test(phil->prev); // Update to check the previous philosopher to avoid circular waiting
 
     pthread_mutex_unlock(&mutex);
 }
 
-void *philosopher(void *num)
+void *philosopher(void *philosopher)
 {
+    struct Philosopher *phil = (struct Philosopher *)philosopher;
+
     while (1)
     {
-        int *i = num;
+        thinking(phil->id);
 
-        thinking(*i);
+        take_fork(phil);
 
-        take_fork(*i);
+        // Now acquire the bowl lock
+        pthread_mutex_lock(&bowl_locks[phil->id]);
 
-        eating(*i);
+        eating(phil->id);
 
-        put_fork(*i);
+        // Release the bowl lock
+        pthread_mutex_unlock(&bowl_locks[phil->id]);
 
-        pthread_mutex_lock(&bowl_mutex);
-        bowls_available++;
+        put_fork(phil);
+
+        // Signal that a bowl is available
+        pthread_mutex_lock(&mutex);
         pthread_cond_signal(&bowl_cond);
-        pthread_mutex_unlock(&bowl_mutex);
+        pthread_mutex_unlock(&mutex);
     }
+}
+
+void initialize_philosophers()
+{
+    philosophers = NULL;
+
+    for (int i = 0; i < N; i++)
+    {
+        pthread_mutex_init(&bowl_locks[i], NULL);
+    }
+
+    for (int i = N - 1; i >= 0; i--)
+    {
+        struct Philosopher *new_philosopher = malloc(sizeof(struct Philosopher));
+        new_philosopher->id = i;
+        pthread_cond_init(&new_philosopher->cond, NULL);
+        new_philosopher->next = philosophers;
+        new_philosopher->prev = NULL;
+
+        if (philosophers != NULL)
+        {
+            philosophers->prev = new_philosopher;
+        }
+
+        pthread_create(&new_philosopher->thread_id, NULL, philosopher, new_philosopher);
+
+        philosophers = new_philosopher;
+    }
+
+    // Update the prev pointer of the last philosopher to complete the doubly linked circular list
+    struct Philosopher *temp = philosophers;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = philosophers;
+    philosophers->prev = temp;
 }
 
 int main()
 {
-    pthread_t thread_id[N];
+    pthread_mutex_init(&mutex, NULL); // Initialize the global mutex
+    pthread_cond_init(&bowl_cond, NULL); // Initialize the bowl condition variable
 
-    // initialize the mutex and conditions
-    pthread_mutex_init(&mutex, NULL);
-    pthread_mutex_init(&bowl_mutex, NULL);
-    pthread_cond_init(&bowl_cond, NULL);
+    initialize_philosophers();
 
-    for (int i = 0; i < N; i++)
-        pthread_cond_init(&cond[i], NULL);
-
-    for (int i = 0; i < N; i++)
+    // Join all threads in the circular linked list
+    struct Philosopher *temp = philosophers;
+    do
     {
-        pthread_create(&thread_id[i], NULL, philosopher, &phil[i]);
-        // printf("Philosopher %d is thinking\n", i + 1);
-    }
+        pthread_join(temp->thread_id, NULL);
+        temp = temp->next;
+    } while (temp != philosophers);
 
-    for (int i = 0; i < N; i++)
-        pthread_join(thread_id[i], NULL);
-
-    // destroy mutex and conditions
+    // destroy mutex, conditions, and locks
     pthread_mutex_destroy(&mutex);
-    pthread_mutex_destroy(&bowl_mutex);
     pthread_cond_destroy(&bowl_cond);
     for (int i = 0; i < N; i++)
-        pthread_cond_destroy(&cond[i]);
+    {
+        pthread_mutex_destroy(&bowl_locks[i]);
+    }
+
+    // Clean up the philosophers
+    temp = philosophers;
+    do
+    {
+        struct Philosopher *next_philosopher = temp->next;
+        free(temp);
+        temp = next_philosopher;
+    } while (temp != philosophers);
 
     return 0;
 }
